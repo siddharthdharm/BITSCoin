62df22264b72f7f242d1c1ddb63286bb34a48a5c
Siddharth Dharm <siddharthdharm@gmail.com>
2018-02-23 19:07:17 +0530

    Transferring coins method added

--- BITSCoin.sol
+++ BITSCoin.sol
@@ -24,8 +24,8 @@ contract BITSCoin {
     event Transfer (address indexed from, address indexed to, uint256 value);
     event Mint(address indexed from, uint256 value);
 
-    //The updateSupply() function
-    function updateSupply() internal returns (unint 256){
+    //The updateAccount() method
+    function updateAccount() internal returns (unint 256){
 
     	if(now - lastHalving >= 2100000 minutes) {
     		reward /= 2;                              // Halving the record after every 210000 minutes
@@ -38,10 +38,51 @@ contract BITSCoin {
 	        lastIncrease = now;
       }
 
-      circulating_supply = spendable_supply - unspent_supply;
-
-      return circulating_supply;
+      //circulating_supply = spendable_supply - unspent_supply;
+      return (spendable_supply - unspent_supply);
     }
 
 
+    // Method for Transferring Coins
+    function transfer(address _to, uint256 transfer_amount) public {
+        require(balanceOf[msg.sender] >= transfer_amount);           // Check if the sender has enough coins
+        require(balanceOf[_to] + transfer_amount >= balanceOf[_to]); // Check for overflows
+        balanceOf[_to] += transfer_amount;                           // Add coins to the recipient
+        balanceOf[msg.sender] -= transfer_amount;                    // Subtract coins from the sender
+        updateAccount();
+
+        Transfer(msg.sender, _to, transfer_amount);                  // Transfer Completed Notification
+    }
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
